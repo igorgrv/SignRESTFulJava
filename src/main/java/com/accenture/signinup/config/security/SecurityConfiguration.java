79217fc994155249c581df06f5db88aa025c7576
Vilela <867695@cognizant.com>
2020-10-10 21:46:46 -0300

    Autorizando health

--- src/main/java/com/accenture/signinup/config/security/SecurityConfiguration.java
+++ src/main/java/com/accenture/signinup/config/security/SecurityConfiguration.java
@@ -45,6 +45,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, "/health").permitAll()
 			.antMatchers(HttpMethod.POST, "/auth").permitAll()
 			.antMatchers(HttpMethod.POST, "/signup").permitAll()
 			.anyRequest().authenticated()
